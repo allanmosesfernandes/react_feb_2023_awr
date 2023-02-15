2c241eac8de80d906f18bc4d82682c8d1521e4fa
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-15 17:24:10 +0000

    first-fold-res

--- src/components/FirstFold/FirstFold.jsx
+++ src/components/FirstFold/FirstFold.jsx
@@ -16,24 +16,24 @@ const FirstFold = () => {
             <strong>INSIDE THE HOUSE:</strong>
             <span>FASHION SHOW</span>
           </div>
-        </div>
-      <div className="future-of-fashion">
+        </div> 
+       <div className="future-of-fashion">
         <p>the future of <br />fashion</p>
       </div>
 
       <div className="body-copy">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sunt distinctio error repellat. Libero dolor tenetur aliquid mollitia animi, dolore quidem repellendus quisquam asperiores maxime. Nemo officiis nulla corporis dolores.
-      </div>
+      </div> 
       </div>
       <div className="image__center">
         <img src={ankan} alt="ankan" className='ankan_img'/>
         <span>
           <img src={thread} alt="thread" className='thread'/>
-        </span>
+        </span> 
       </div>
       <div className="photo__by">
-        <p>PHOTO BY:</p>
-        
+        <strong>PHOTO BY:</strong>
+        <span>Pravin Kumar</span>
       </div>
 
     </div>
