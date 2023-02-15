1c9576d99ff5dddc9ec843be423adb8256fbadcd
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-15 14:36:18 +0000

    first-fold-init

--- src/components/FirstFold/FirstFold.jsx
+++ src/components/FirstFold/FirstFold.jsx
@@ -4,6 +4,25 @@ import "./firstfold.scss";
 const FirstFold = () => {
   return (
     <div className='wrapper first-fold'>
+      <div className="text__left">
+        <div className="picture__details">
+          <div className="picture__details_left">
+            <strong>COLLABORATION WITH:</strong>
+            <span>GUCCI HANDBAGS</span>
+          </div>
+          <div className="picture__details_right">
+            <strong>INSIDE THE HOUSE:</strong>
+            <span>FASHION SHOW</span>
+          </div>
+        </div>
+      <div className="future-of-fashion">
+        <p>the future of <br />fashion</p>
+      </div>
+
+      <div className="body-copy">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sunt distinctio error repellat. Libero dolor tenetur aliquid mollitia animi, dolore quidem repellendus quisquam asperiores maxime. Nemo officiis nulla corporis dolores.
+      </div>
+      </div>
 
     </div>
   )
