15e46aba977cb70fcc755600598e8b0f404f3ace
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-15 23:47:18 +0000

    second_fold

---
+++ src/components/SecondFold/secondFold.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import "./second-fold.scss";
+
+const secondFold = () => {
+  return (
+    <div className='wrapper second__fold'>
+        <div className="attribute__card__container">
+            <div className="attribute__card__white">
+                <div className="attribute__card__details">
+                    <div className="details__left">
+                        <strong>Lorem ipsum dolor sit amet</strong>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                    <div className="details__right">
+                        <div className="status__div">
+
+                        </div>
+                    </div>
+                </div>
+                <h4>Fashion Merchandiser</h4>
+                <button className="button-74">LEARN MORE </button>
+            </div>
+            <div className="attribute__card__black">
+                                <div className="attribute__card__details">
+                    <div className="details__left">
+                        <strong>Lorem ipsum dolor sit amet</strong>
+                        <p>Lorem Ipsum</p>
+                    </div>
+                    <div className="details__right">
+                        <div className="status__div">
+
+                        </div>
+                    </div>
+                </div>
+                <h4>Fashion Merchandiser</h4>
+                <button className="button-74">LEARN MORE </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default secondFold
