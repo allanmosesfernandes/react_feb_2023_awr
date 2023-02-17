3fd725187bed87200e030784ac7a19bb8187ee59
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-17 12:31:08 +0000

    writing-cursive

--- src/components/Writing/writing.jsx
+++ src/components/Writing/writing.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./writing.scss";
+
+
 const Writing = () => {
   return (
-    <div className="wrapper writing__fold">
-        <p className="future-of-fashion">writings</p>
+    <div className=" writing__fold">
+        <p className="cursive">Writing Blog</p>
+        <p className="blog__body__copy">
+          I've got 
+          <span></span>
+        </p>
+        <p className="blog__body__copy">
+          <span className="cursive__inline">exciting insight </span>for you
+        </p>
     </div>
   )
 }
