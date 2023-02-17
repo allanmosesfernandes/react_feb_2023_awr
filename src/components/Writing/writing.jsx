ca4f8940af390aaf9bf4f21a11ce82a2bcd94d55
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-17 16:47:12 +0000

    blog_url

--- src/components/Writing/writing.jsx
+++ src/components/Writing/writing.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import "./writing.scss";
 import Star from "../../assets/images/star.png";
 import BlogImage from "../../assets/images/blog_preview.webp";
 import axios from 'axios';
 
+
+const Writing = () => {
+const [posts, getPosts] = useState([]);
+
+useEffect(() => {
 axios.get('http://allanmoses.ninja/wp-json/wp/v2/categories?slug=homepage')
   .then(response => {
     const homepageCategoryId = response.data[0].id;
@@ -11,14 +16,16 @@ axios.get('http://allanmoses.ninja/wp-json/wp/v2/categories?slug=homepage')
     // Do something with the response data
   })
   .then(response => {
-    console.log(response.data);
+    getPosts(response.data)
     // Do something with the filtered posts
   })
   .catch(error => {
     console.error(error);
   });
 
-const Writing = () => {
+}, [])
+
+
   return (
     <div className=" writing__fold">
         <p className="cursive">Writing Blog</p>
@@ -31,37 +38,25 @@ const Writing = () => {
         </p>
 
         <div className="blog__preview__container">
+        {
+          posts.map((post,index) => {
+          const featuredMediaId = post.featured_media;
+            console.log(featuredMediaId);
+            return <div className="blog__preview__div" key={index}>
+              
+                  {/* <img src={imageURL} alt="" /> */}
+                  <p className="blog__preview__title">
+                    5 Ways to Find Content Ideas For Social Media
+                  </p>
+                  <div className="blog__preview__bottom">
+                    <div className="blog__preview__date">- February 9, 2023</div>
+                    <div className="blog__preview__category">Marketing</div>
+                  </div>
+          </div>
+          })
+        }
 
-          <div className="blog__preview__div">
-            <img src={BlogImage} alt="" />
-            <p className="blog__preview__title">
-              5 Ways to Find Content Ideas For Social Media
-            </p>
-            <div className="blog__preview__bottom">
-              <div className="blog__preview__date">- February 9, 2023</div>
-              <div className="blog__preview__category">Marketing</div>
-            </div>
-          </div>
-          <div className="blog__preview__div">
-            <img src={BlogImage} alt="" />
-            <p className="blog__preview__title">
-              5 Ways to Find Content Ideas For Social Media
-            </p>
-            <div className="blog__preview__bottom">
-              <div className="blog__preview__date">- February 9, 2023</div>
-              <div className="blog__preview__category">Marketing</div>
-            </div>
-          </div>
-          <div className="blog__preview__div">
-            <img src={BlogImage} alt="" />
-            <p className="blog__preview__title">
-              5 Ways to Find Content Ideas For Social Media
-            </p>
-            <div className="blog__preview__bottom">
-              <div className="blog__preview__date">- February 9, 2023</div>
-              <div className="blog__preview__category">Marketing</div>
-            </div>
-          </div>
+
 
 
         </div>
