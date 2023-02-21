a0699162123f18b6ee4fdb537433e0b4d83fa745
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-21 11:19:51 +0000

    first-fold-cleanup

--- src/components/SecondFold/secondFold.jsx
+++ src/components/SecondFold/secondFold.jsx
@@ -4,28 +4,29 @@ import "./second-fold.scss";
 const SecondFold = () => {
   return (
     <div className='wrapper second__fold'>
+        <h4>What I do</h4>
         <div className="attribute__card__container">
             <div className="attribute__card__black">
                 <div className="attribute__card__details">
                     <div className="details__left">
-                        <strong>Lorem ipsum dolor  </strong>
-                        <p>Lorem Ipsum</p>
+                        {/* <strong>Lorem ipsum dolor  </strong>
+                        <p>Lorem Ipsum</p> */}
                     </div>
                     <div className="details__right">
                         <div className="status__div">
-                            <div className="glowing-dot"></div>
 
                         </div>
                     </div>
                 </div>
-                <h4 className="attribute__post">Fashion Merchandiser</h4>
-                <button className="button-74">LEARN MORE </button>
+                <h4 className="attribute__post"><span className='gothic'>Fashion</span> Merchandiser</h4>
+                <button className="button-50">Learn More</button>
+
             </div>
             <div className="attribute__card__white">
-                                <div className="attribute__card__details">
+                <div className="attribute__card__details">
                     <div className="details__left">
-                        <strong>Lorem ipsum dolor  </strong>
-                        <p>Lorem Ipsum</p>
+                        {/* <strong>Lorem ipsum dolor  </strong>
+                        <p>Lorem Ipsum</p> */}
                     </div>
                     <div className="details__right">
                         <div className="status__div">
@@ -33,8 +34,8 @@ const SecondFold = () => {
                         </div>
                     </div>
                 </div>
-                <h4 className='attribute__post'>Trend Forecaster</h4>
-                <button className="button-74">LEARN MORE </button>
+                <h4 className='attribute__post'><span className='gothic'>Trend </span>Forecaster</h4>
+                <button className="button-50">Learn More</button>
                 {/* <FontAwesomeIcon icon="fa-solid fa-arrow-right-long" /> */}
             </div>
         </div>
