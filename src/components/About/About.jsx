1e8e8c088549120a39461192fac167031221e5dc
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-28 12:35:01 +0000

    second-fold

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,17 +5,21 @@ const About = () => {
   return (
     <div className='wrapper about_me'>
         <div className="about__me__second__fold">
-            <h2>Hi there, I'm Ankan.</h2>
+            <h2>Hi there, I'm <span>Ankan</span>.</h2>
             <div className="second_fold_block">
-                <button className='about_me_btn'>Also Chittu, or Meow</button>
+                <div>
+            <button className='about_me_btn'>Also Chittu, or Meow</button> 
+                </div>
+                
+                <div className="about__me__txt">
                 <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, error.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, error.
+                 An LA-based award-winning design director and UI/UX design expert with 10 years of experience in design and management.
                 </p>
                 <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, error.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, error.
+                I am a mom to a beautiful 2 y.o. daughter who already loves attending business meetings with me. Also, a big part of my life is Netrix - a design agency which I run together with my husband.
                 </p>
+                </div>
+
             </div>
         </div>
 
