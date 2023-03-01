ad34441ac1cb35727b2e4d413a54ca1a7bd91b5d
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-01 16:32:52 +0000

    visual musings

--- src/components/WeeklyInspiration/WeeklyInspiration.jsx
+++ src/components/WeeklyInspiration/WeeklyInspiration.jsx
@@ -10,9 +10,20 @@ const WeeklyInspiration = () => {
       .then(data => setImages(data))
       .catch(error => console.error(error));
   }, []);
-
   return (
+    <div className='weekly__inspiration wrapper'>
     <div>WeeklyInspiration</div>
+    <div className="image__grid__container">
+        {
+            images.map((image,index) => {
+                let imageURL = image.image_url;
+                return <div key={index} className="image__grid__images">
+                    <img src={imageURL} alt="index" id={index}/>
+                </div>
+            })
+        }
+    </div>
+</div>
   )
 }
 
