1709a6c2f39ee59a32fab8f8f676f0975c28c469
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-02 15:00:39 +0000

    first-fold

--- src/components/WeeklyInspiration/WeeklyInspiration.jsx
+++ src/components/WeeklyInspiration/WeeklyInspiration.jsx
@@ -13,26 +13,26 @@ const WeeklyInspiration = () => {
   return (
     <div className='weekly__inspiration '>
     <h2>Visual Musings</h2>
-    <p>What's been inspiring me lately</p>
-<div className="image__grid__container">
-  {images.map((image, index) => {
-    let imageURL = image.image_url;
-    let group = index % 5; 
-    const excerptElement = image.excerpt.rendered;
-    const excerptText = document.createElement('div');
-    excerptText.innerHTML = excerptElement;
-    const plainExcerpt = excerptText.textContent;
-    // Set CSS classes for each group
-    let highlightImage = group === 0 ? "highlight" : "secondary";
-    let smallest = group === 4 ? "smally" : "" 
+    <p>What's been <span className='sf-italic'>inspiring</span> me lately</p>
+      <div className="image__grid__container">
+        {images.map((image, index) => {
+          let imageURL = image.image_url;
+          let group = index % 5; 
+          const excerptElement = image.excerpt.rendered;
+          const excerptText = document.createElement('div');
+          excerptText.innerHTML = excerptElement;
+          const plainExcerpt = excerptText.textContent;
+          // Set CSS classes for each group
+          let highlightImage = group === 0 ? "highlight" : "secondary";
+          let smallest = group === 4 ? "smally" : "" 
 
-    return (
-      <div key={index} className={`image__grid__images ${highlightImage} ${smallest}`}>
-        <img src={imageURL} alt={`imageTitle`} title={plainExcerpt} />
+          return (
+            <div key={index} className={`image__grid__images ${highlightImage} ${smallest}`}>
+              <img src={imageURL} alt={`imageTitle`} title={plainExcerpt} />
+            </div>
+          );
+        })}
       </div>
-    );
-  })}
-</div>
 
 </div>
   )
