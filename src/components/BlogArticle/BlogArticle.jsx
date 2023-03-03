7cd8b111dd2ba71e007172bb9f25857319d23f60
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-03 13:46:02 +0000

    related articles fix

--- src/components/BlogArticle/BlogArticle.jsx
+++ src/components/BlogArticle/BlogArticle.jsx
@@ -17,17 +17,17 @@ const BlogArticle = () => {
     axios.get(`https://ankanchittalipi.com/wp-json/wp/v2/posts?slug=${slug}&_embed`)
       .then(response => {
         setPost(response.data[0]);
+        axios.get(`https://ankanchittalipi.com/wp-json/wp/v2/posts?exclude=${response.data[0].id}&per_page=3&_embed`)
+          .then(response => {
+            setMorePosts(response.data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
       })
       .catch(error => {
         console.error(error);
       });
-  axios.get(`https://ankanchittalipi.com/wp-json/wp/v2/posts?exclude=${post ? post.id : ''}&per_page=3&_embed`)
-    .then(response => {
-      setMorePosts(response.data);
-    })
-    .catch(error => {
-      console.error(error);
-    });
   }, [slug]);
 
 
