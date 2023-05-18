f1918746ebe00f2e19b19a71f26b05cc459e56f7
Allan <fernzallan14@gmail.com>
2023-05-18 14:59:17 +0100

    Date-change on Blog listing

--- src/components/BlogListing/BlogListing.jsx
+++ src/components/BlogListing/BlogListing.jsx
@@ -10,6 +10,32 @@ const BlogListing = () => {
 
   const navigate = useNavigate();
 
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const suffix = getOrdinalSuffix(day);
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = date.toLocaleDateString('en-US', options);
+    return formattedDate.replace(`${day}`, `${day}${suffix}`);
+  }
+  
+  function getOrdinalSuffix(day) {
+    if (day >= 11 && day <= 13) {
+      return 'th';
+    }
+  
+    const lastDigit = day % 10;
+    switch (lastDigit) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  }
   
  useEffect(() => {
   window.scrollTo(0,0);
@@ -34,6 +60,7 @@ const BlogListing = () => {
           {posts.map((post, index) => {
             const imageUrl = post.jetpack_featured_media_url;
             const year = post.date.split("-")[0];
+            const formattedDate = formatDate(post.date);
 
             return (
               <div className="blog__listing__div" key={index} onClick={() => navigate(`/blog/${post.slug}`)}>
@@ -42,7 +69,7 @@ const BlogListing = () => {
                   <img src={imageUrl} alt="" />
                 </div>
                 <div className="listing__details">
-                  <p className="listing__date">{year}</p>
+                  <p className="listing__date">{formattedDate}</p>
                   <h3 className="listing-title">{post.title.rendered}</h3>
                 </div>
               </div>
